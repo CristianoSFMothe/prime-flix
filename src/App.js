661180188e9b3037fa7bd6e5dc0d131e8dda9bbc
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-01-12 16:43:58 -0300

    feat: reset the default CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import RouteApp from './routes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 function App() {
   return (
     <div className="App">
-      <ToastContainer autoClose={3000} />
-      <RouteApp />
-
+      <h1>Teste</h1>
     </div>
   );
 }
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-// Base URL https://api.themoviedb.org/3/
-// URL da API https://api.themoviedb.org/3/movie/now_playing?api_key=7c9d823bb01b90283436e16503b62a08&language=pt-BR
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/',
-});
-
-export default api;
